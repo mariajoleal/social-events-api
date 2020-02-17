398b70a5522daec770ab97859b2a61b149c3975a
Mariajo Leal <mariajo20leal@gmail.com>
2020-02-17 16:57:34 -0600

    Changed DB String info

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
-  mongoURI: "mongodb://mariajoleal:mjlr200495@ds261486.mlab.com:61486/social-events", 
+  mongoURI: "mongodb://user:userpassword@ds261486.mlab.com:61486/social-events", 
   secretOrKey: "secret"
 };
 
